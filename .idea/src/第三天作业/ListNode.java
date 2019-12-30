babd75affbbc94fad1a1e29c23f17bddc7075787
Eric <719320312@qq.com>
2019-12-30 09:26:46 +0800

    第三天作业

--- .idea/src/第三天作业/ListNode.java
+++ .idea/src/第三天作业/ListNode.java
@@ -2,8 +2,8 @@ package 第三天作业;
 
 public class ListNode {
     int val;
-    ListNode next;
-    ListNode(int x){
-        this.val = x;
+    ListNode next = null;
+    ListNode(int val){
+        this.val = val;
     }
 }
--- .idea/src/第三天作业/Solution.java
+++ .idea/src/第三天作业/Solution.java
@@ -1,9 +1,9 @@
 package 第三天作业;
 /*public class ListNode{
     int val;
-    ListNode next;
-    ListNode(int x){
-        this.val = x;
+    ListNode next = null;
+    ListNode(int val){
+        this.val = val;
     }
 }*/
  public class Solution {
