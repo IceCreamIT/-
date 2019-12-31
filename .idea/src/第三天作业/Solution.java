76f58e33a4ea58a63bd282c6562185608ceb6d57
Eric <719320312@qq.com>
2019-12-31 10:33:38 +0800

    第四天作业

--- .idea/src/第三天作业/Solution.java
+++ .idea/src/第三天作业/Solution.java
@@ -12,7 +12,7 @@ package 第三天作业;
         if(list2 == null) return list1;
         ListNode merge = null;
 
-        if(list1.val<list2.val){
+        /*if(list1.val<list2.val){
             merge = list1;
             merge.next = mergeTwoList(list1.next,list2);
         }else if(list1.val==list2.val){
@@ -21,7 +21,7 @@ package 第三天作业;
         }else{
             merge = list2;
             merge.next = mergeTwoList(list1,list2.next);
-        }
+        }*/
         return merge;
     }
 }
---
+++ .idea/src/第四天作业/Solution.java
@@ -0,0 +1,17 @@
+package 第四天作业;
+
+public class Solution {
+    public int [] merge(int []nums1, int m,int []nums2, int n){
+        int p = m-- + --n -1;
+        while(m >= 0 && n >= 0){
+              nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+
+        while(n >= 0){
+            nums1[p--] = nums2[n--];
+        }
+
+        return nums1;
+
+    }
+}
